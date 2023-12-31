dfbc1bdced7c55c06abb9841831be527704d60bf
Lasitha Weligampola <lasiprabo@gmail.com>
2023-12-31 14:30:49 -0500

    debugging

--- utils/spotify.ts
+++ utils/spotify.ts
@@ -27,7 +27,7 @@ async function getAuthorizationToken() {
     body,
   }).then((r) => r.json());
 
-  console.log(response, Authorization)
+  console.log(response, Authorization, process.env )
   return `Bearer ${response.access_token}`;
 }
 
